4e70fd6a1c20d6dbbebe44aafbff52fba52b4b66
DictatorXBlade <venkat.darkwing@gmail.com>
2015-07-28 13:39:29 +0530

    Bugfixes
    Fixed all remaining bugs.

--- Server.js
+++ Server.js
@@ -2,49 +2,52 @@ net = require('net');
 fs = require('fs');
 path = require('path');
 spawn=require('child_process').spawn;
+readline=require('readline');
 function Queue(i){ //circular queue of integers of size i
-	var a=new Array(i),front=-1,back=-1;
+	this.a=new Array(i);
+	this.front=-1;
+	this.back=-1;
 	this.enqueue=function(data) {
-		if (front==-1) { //queue is empty
-			front=0;
-			back=0;
-			a[back]=data;
+		if (this.front==-1) { //queue is empty
+			this.front=0;
+			this.back=0;
+			this.a[this.back]=data;
 		}
-		else if ((front==0 and back==i-1) or back==front-1) { //queue full
+		else if ((this.front==0 && this.back==i-1) || this.back==this.front-1) { //queue full
 			;
 		}
-		else if (back==i-1) { //wrap around
-			a[back]=data;
-			back=0;
+		else if (this.back==i-1) { //wrap around
+			this.back=0;
+			this.a[this.back]=data;
 		}
 		else { //regular case
-			a[back]=data;
-			++back;
+			++this.back;
+			this.a[this.back]=data;
 		}
 	}
 	this.dequeue=function(data) {
-		if (front==-1) { //queue is empty
+		if (this.front==-1) { //queue is empty
 			return null;
 		}
-		else if (front==back) { //queue has 1 element
-			temp=a[front];
-			front=-1;
-			back=-1;
+		else if (this.front==this.back) { //queue has 1 element
+			temp=this.a[this.front];
+			this.front=-1;
+			this.back=-1;
 			return temp;
 		}
-		else if (front==i-1) { //wrap around
-			temp=a[front];
-			front=0;
+		else if (this.front==i-1) { //wrap around
+			temp=this.a[this.front];
+			this.front=0;
 			return temp;
 		}
 		else { //regular case
-			temp=a[front];
-			++front;
+			temp=this.a[this.front];
+			++this.front;
 			return temp;
 		}
 	}
 	this.empty=function() {
-		if (front==-1) {
+		if (this.front==-1) {
 			return true;
 		}
 		else {
@@ -55,7 +58,7 @@ function Queue(i){ //circular queue of integers of size i
 var connections = new Object();
 //read all config data from file
 var config = JSON.parse(fs.readFileSync(path.join(process.cwd(),'server_config.txt')).toString())
-game_ids=Queue(config["Max_Games"])
+var game_ids=new Queue(config["Max_Games"])
 for (i=0;i!=config["Max_Games"];++i) {
 	game_ids.enqueue(i);
 }
@@ -71,8 +74,8 @@ var play_game_map=new Object();
 };*/
 function game_inserter_output(i) {
 	return function(data) { //sends data from game i to the player who is first part of string(no spaces)
-		var split_data=data.split(" ")
-		connections[split_data[0]].write(i.toString()+" "+split_data.slice(1,split_data.length).join(" "));
+		var split_data=data.toString().split(" ");
+		connections[split_data[0]].write(i.toString()+" "+split_data.slice(1,split_data.length).join(" ")+"\r\n");
 	}
 }
 function game_deleter(i) {
@@ -88,46 +91,51 @@ function game_deleter(i) {
 }
 server=net.createServer(function (socket) {
 	socket.name = socket.remoteAddress + ":" + socket.remotePort //gives socket a name
+	socket.write("Hello "+socket.name+"\r\n")
 	//in the future, there should be a way to give a username(and maybe password)
 	connections[socket.name]=socket //inserts socket into the table of players
 	play_game_map[socket.name]=[] //the player is presently playing 0 games
-	console.log(socket.name)
   // Handle incoming messages from connections.
   socket.on('data', function (data) {
 	var local=data.toString().split(" ");
-	if (local[0]=="0") { //game create command
+	var command=parseInt(local[0])
+	if (command==0) { //game create command
 		if (game_ids.empty()||config["Max_Games_per_Player"]==play_game_map[socket.name].length) { //maximum number of games has already been created
 			socket.write("REJECTED");
 		}
 		else {
 			var args=local.slice(1,local.length); //command line args(usually clauses, and the host player's state) for the game
+			args.unshift(socket.name)
 			var idx=game_ids.dequeue();
 			play_game_map[socket.name].push(idx);
 			game_table[idx]=spawn(config["Game_Command"],args,
 			{
 				stdio : ['pipe','pipe','pipe']
-			}); //add entry to the game table
+			}); //add entry to the game table.
 			socket.write("ACCEPTED");
 			game_table[idx].on('exit',game_deleter(idx));
-			game_table[idx].stdout.on('data',game_inserter_output(idx));
+			rl=readline.createInterface({input : game_table[idx].stdout , output : null});
+			rl.on('line',game_inserter_output(idx));
+			//game_table[idx].stdout.on('data',game_inserter_output(idx));
 			//game_table[idx].stderr.on('data',game_inserter_output(idx)); //handling errors in games is for the future
 		}
 	}
-	else if (local[0]=="1") { //game join command
-		var game_id=parseInt(local[1])
+	else if (command==1) { //game join command
+		game_id=parseInt(local[1])
 		if (game_id in game_table && play_game_map[socket.name].length<config["Max_Games_per_Player"]&&play_game_map[socket.name].indexOf(game_id)==-1) {
 			//if the game actually exists, the player hasn't exceeded quota, and the player isn't already in the game
-			game_table[game_id].write(socket.name+" "+local.slice(2,local.length).join(" "));
+			socket.write("ACCEPTED")
+			game_table[game_id].stdin.write(socket.name+" "+local.slice(2,local.length).join(" "));
 			play_game_map[socket.name].push(game_id)
 		}
 		else {
 			socket.write("REJECTED");
 		}
 	}
-	else if (local[0]=="2") { //game transmit command
+	else if (command==2) { //game transmit command
 		var game_id=parseInt(local[1])
 		if (game_id in game_table && play_game_map[socket.name].indexOf(game_id)!=-1) { //if the game actually exists, and the player is part of it
-			game_table[game_id].write(socket.name+" "+local.slice(2,local.length).join(" "));
+			game_table[game_id].stdin.write(socket.name+" "+local.slice(2,local.length).join(" "));
 		}
 	}
   });
@@ -136,7 +144,7 @@ server=net.createServer(function (socket) {
   socket.on('end', function () {
     delete connections[socket.name];
 	play_game_map[socket.name].forEach(function(entry){
-		game_table[entry].write(socket.name+" DISCONNECTED"); //inform each game that this player has disconnected
+		game_table[entry].stdin.write(socket.name+" DISCONNECTED"); //inform each game that this player has disconnected
 	});
 	delete play_game_map[socket.name];
   });
